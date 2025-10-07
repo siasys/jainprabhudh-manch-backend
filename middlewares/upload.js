c4d2336828656db1392fb6382db71b380b52f2bd
jainprabuddhmanch <jainprabuddhmanch@gmail.com>
2025-10-07 18:36:15 +0530

    file upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -186,7 +186,7 @@ const upload = multer({
 const handleMulterError = (err, req, res, next) => {
   if (err instanceof multer.MulterError) {
     if (err.code === 'LIMIT_FILE_SIZE') {
-      return res.status(400).json({ error: 'File too large. Maximum size is 30MB.' });
+      return res.status(400).json({ error: 'File too large. Maximum size is 50MB.' });
     }
     if (err.code === 'LIMIT_FILE_COUNT') {
       return res.status(400).json({ error: 'Too many files. Maximum is 10 files.' });
