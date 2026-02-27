6bcc63758427164654016d7381ec4260ab6d4107
jainprabuddhmanch <khetify.beejbhandar@gmail.com>
2026-02-27 16:44:51 +0530

    logger chnages

--- config/logger.js
+++ config/logger.js
@@ -4,11 +4,11 @@ const newrelicFormatter = require("@newrelic/winston-enricher")(winston);
 
 const logger = winston.createLogger({
   level: "info",
-  // New Relic formatter ko yahan add karein
   format: winston.format.combine(newrelicFormatter(), winston.format.json()),
   transports: [
-    new winston.transports.Console(),
-    // Aap chahein toh yahan File transport bhi add kar sakte hain
+    new winston.transports.File({
+      filename: "newrelic_agent.log",
+    }),
   ],
 });
 
