2b00ca41c56a6115f531950ccb2ed5c1aa3d026c
jainprabuddhmanch <khetify.beejbhandar@gmail.com>
2026-06-19 14:54:54 +0530

    route change minority card

--- routes/UserRegistrationRoutes/jainAdharRoute.js
+++ routes/UserRegistrationRoutes/jainAdharRoute.js
@@ -105,7 +105,7 @@ router.get(
 router.get( "/check-duplicate" ,checkApplicationDuplicate);
 // router.get('/applications', getApplicationsReview);
 router.get('/generate-card/:id', generateJainAadharCard);
-router.get("/generate-minority-card/:id", generateMinorityCard);
+router.get("/minority-card/:id", generateMinorityCard);
 router.get('/verify/jain-shravak/:jainAadharNumber', async (req, res) => {
   try {
     const { jainAadharNumber } = req.params;
